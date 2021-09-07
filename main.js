693df546ef61e584f9059a3a37cd28acd3cddb89
Hollman Rojas <hollmanerojas@gmail.com>
2021-09-07 18:28:59 -0500

    bar/feature creation

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //Funcion Anonima
 (function () {
-    //Self se asigna a window
+    //Self se asigna a window.
     self.Board = function (width, height) {
 
         //Variables del objeto
@@ -16,8 +16,8 @@
     self.Board.prototype = {
         get elements() {
             var elements = this.bars;
-            this.elements.push(this.ball);
-            return this.elements;
+            elements.push(this.ball);
+            return elements;
         }
     }
 
@@ -30,25 +30,25 @@
         this.width = width;
         this.height = height;
         this.board = board;
-        //Agregamos en el board el objeto bar.
+        //Agregamos en el board un elemento con push.
         this.board.bars.push(this);
-
-        //Variable para dibujar en canvas
+        //Variable para dibujar un rectangulo en canvas
         this.kind = "rectangle";
+        
 
     }
     //Modificar el prototype de la funcion
     self.Bar.prototype = {
         //Funciones para mover el rectangulo
-        down: function(){
+        down: function () {
 
         },
-        up: function(){
+        up: function () {
 
         }
     }
 })();
-//Clase para crear el tablero
+//Clase para crear el tablero(vista)
 (function () {
     self.BoardView = function (canvas, board) {
         //Variables del objeto
@@ -57,53 +57,45 @@
         this.canvas.height = board.height;
         this.board = board;
         //Objeto contexto para dibujar en JS
-        this.ctx = canvas.getContext("2d")
+        this.ctx = canvas.getContext("2d");
     }
-    //Modificar prototype de la clase BoardView
-    self.BoardView.prototype={
-        //Validar
-        
 
-        draw: function(){
-            for(var i = this.board.elements.length -1; i>=0; i--){
+    //Modificar prototype de la clase BoardView
+    self.BoardView.prototype = {
+        //Validar
+        draw: function () {
+            for (var i = this.board.elements.length - 1; i >= 0; i--) {
                 var drawElement = this.board.elements[i];
 
-                draw(this.ctx,drawElement);
+                draw(this.ctx, drawElement);
+
             };
         }
     }
+
     //Dibujar elementos
-    function draw(ctx, element){
+    function draw(ctx, element) {
         //Validar si el objeto a dibujar tiene una propiedad kind
-        if (element !== null && element.hasOwnProperty("kind")){
-            switch(element.kind){
+        if (element !== null && element.hasOwnProperty("kind")) {
+            switch (element.kind) {
                 case "rectangle":
-                    ctx.fillRect(element.x,element.y,element.width,element.height);
+                    ctx.fillRect(element.x, element.y, element.width, element.height);
                     break;
             }
-            
-        };
-        //Dependiendo del tipo dibuje segun el caso
-        switch(element.kind){
-            case "square":
-                //Dibujar cuadrado
-                ctx.fillRect(element.x, element.y,element.width,element.height);
-                break;
         }
-
     }
 
 })();
 
-window.addEventListener("load", main);
+self.addEventListener("load", main);
 
-
-
-function main() {
-    var bar = new Bar(20, 100, 40, 100,board);
-    var board = new Board(800, 500);
-    const canvas = document.getElementById('canvas')
-    const board_view = new BoardView(canvas, board);
-    //Dibujar todos los elementos
-    board_view.draw();
-}
+    
+    function main() {
+        var board = new Board(800, 500);
+        var bar = new Bar(20, 100, 40, 100, board);
+        const canvas = document.getElementById('canvas')
+        const board_view = new BoardView(canvas, board);
+        //Dibujar todos los elementos
+        board_view.draw();
+        console.log(board);
+    }
