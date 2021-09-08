59d7191f516bfaccfa4636e9dd4e4907dd0fdb92
Hollman Rojas <hollmanerojas@gmail.com>
2021-09-07 23:29:31 -0500

    feature/ ball motion

--- main.js
+++ main.js
@@ -10,12 +10,13 @@
         this.game_over = false;
         this.bars = [];
         this.ball = null;
+        this.playing = false;
 
     }
     //Metodo para mostrar barras y esferas en el objeto Board
     self.Board.prototype = {
         get elements() {
-            var elements = this.bars.map(function(bar){return bar;});
+            var elements = this.bars.map(function (bar) { return bar; });
             elements.push(this.ball);
             return elements;
         }
@@ -23,17 +24,27 @@
 
 })();
 //Metodo para crear el objeto pelota
-(function (){
-    self.Ball = function(x,y,radius,board){
-    this.x = y;
-    this.y = y;
-    this.radius = radius;
-    this.speed_y = 0;
-    this.speed_x = 3
-    this.board = board;
+(function () {
+    self.Ball = function (x, y, radius, board) {
+        this.x = y;
+        this.y = y;
+        this.radius = radius;
+        this.speed_y = 0;
+        this.speed_x = 3
+        this.board = board;
+        this.direction = 1;
 
-    board.ball = this;
-    this.kind = "circle";
+        board.ball = this;
+        this.kind = "circle";
+
+
+    }
+    self.Ball.prototype = {
+        move: function () {
+            this.x += (this.speed_x * this.direction);
+            this.y += (this.speed_y);
+
+        }
     }
 })();
 
@@ -97,10 +108,15 @@
             };
         },
         play: function () {
-            //Limpiar Board
-            this.clean();
-            //Dibujar Board 
-            this.draw();
+            if (this.board.playing) {
+                //Limpiar Board
+                this.clean();
+                //Dibujar Board 
+                this.draw();
+                //Moviento de la pelota
+                this.board.ball.move();
+            }
+
         }
     }
 
@@ -114,7 +130,7 @@
                 break;
             case "circle":
                 ctx.beginPath();
-                ctx.arc(element.x, element.y, element.radius, 0,7);
+                ctx.arc(element.x, element.y, element.radius, 0, 7);
                 ctx.fill();
                 ctx.closePath();
                 break;
@@ -137,28 +153,38 @@ var ball = new Ball(350, 100, 10, board);
 //Funcion de moviento de barras
 document.addEventListener("keydown", function (ev) {
     //cancelar movimiento en el navegador
-    ev.preventDefault();
+
 
     if (ev.keyCode == 38) {
+        ev.preventDefault();
         bar.up();
     } else if (ev.keyCode == 40) {
+        ev.preventDefault();
         bar.down();
 
         //Asignacion de teclas a la barra numero 2 (S,W)
     } else if (ev.keyCode == 87) {
+        ev.preventDefault();
         //W
         bar_2.up();
     } else if (ev.keyCode === 83) {
+        ev.preventDefault();
         //S
         bar_2.down();
+    } else if (ev.keyCode == 32) {
+        ev.preventDefault();
+        board.playing = !board.playing;
     }
 
 });
 
-//self.addEventListener("load", main);
+board_view.draw();
 
 //Request Animation frame
 window.requestAnimationFrame(controller);
+setTimeout(function () {
+    this.ball.direction = -1;
+}, 4000); 
 
 function controller() {
 
