2b3210b5e94f6c6e298233aed0730ca1d6bf0aca
Hollman Rojas <hollmanerojas@gmail.com>
2021-09-08 00:33:37 -0500

    feature/ collision ball with bars

--- main.js
+++ main.js
@@ -26,23 +26,50 @@
 //Metodo para crear el objeto pelota
 (function () {
     self.Ball = function (x, y, radius, board) {
-        this.x = y;
+        this.x = x;
         this.y = y;
         this.radius = radius;
         this.speed_y = 0;
         this.speed_x = 3
         this.board = board;
         this.direction = 1;
+        this.bounce_angle = 0;
+        this.max_bounce_angle = Math.PI / 12;
+        this.speed = 3;
 
         board.ball = this;
         this.kind = "circle";
 
 
     }
+    //Metodo para definir moviento y colisión de pelota.
     self.Ball.prototype = {
         move: function () {
             this.x += (this.speed_x * this.direction);
             this.y += (this.speed_y);
+
+        },
+        get width(){
+            return this.radius * 2;
+        },
+        get height(){
+            return this.radius * 2;
+        },
+        collision: function (bar) {
+            //Reacciona a la colisión con una barra que recibe como parametro
+            var relative_intersect_y = (bar.y + (bar.height / 2)) - this.y;
+
+            var normalized_intersect_y = relative_intersect_y / (bar.height / 2);
+
+            this.bounce_angle = normalized_intersect_y * this.max_bounce_angle;
+
+            //Calcula velocidad tanto x como en y
+            this.speed_y = this.speed * -Math.sin(this.bounce_angle);
+            this.speed_x = this.speed * Math.cos(this.bounce_angle);
+
+            //Cambiar la direccion según la posicion de la barra
+            if (this.x > (this.board.width / 2)) this.direction = -1;
+            else this.direction = 1;
 
         }
     }
@@ -107,23 +134,58 @@
 
             };
         },
+        check_collisions: function () {
+            //iterar las barras
+            for (var i = this.board.bars.length - 1; i >= 0; i--) {
+                var bar = this.board.bars[i];
+                if (hit(bar, this.board.ball)) {
+                    this.board.ball.collision(bar);
+                }
+            }
+        },
         play: function () {
             if (this.board.playing) {
                 //Limpiar Board
                 this.clean();
                 //Dibujar Board 
                 this.draw();
-                //Moviento de la pelota
+                //Colición
+                this.check_collisions();
+                //Movimiento de la pelota
                 this.board.ball.move();
             }
 
         }
     }
 
+    function hit(a, b) {
+        //Revisa si a colisiona con b.
+        var hit = false;
+        //Colisiones horizontales
+        if (b.x + b.width >= a.x && b.x < a.x + a.width) {
+            //Colisiones verticales
+            if (b.y + b.height >= a.y && b.y < a.y + a.height)
+                hit = true;
+        }
+        //Colisión de a con b
+        if (b.x <= a.x && b.x + b.width >= a.x + a.width) {
+            if (b.y <= a.y && b.y + b.height >= a.y + a.height)
+                hit = true;
+        }
+        //Colisión de b con a
+        if (a.x <= b.x && a.x + a.width >= b.x + b.width) {
+            if (a.y <= b.y && a.y + a.height >= b.y + b.height)
+                hit = true;
+        }
+        return hit;
+
+
+    }
+
     //Dibujar elementos
     function draw(ctx, element) {
         //Validar si el objeto a dibujar tiene una propiedad kind
-
+        //background(220);
         switch (element.kind) {
             case "rectangle":
                 ctx.fillRect(element.x, element.y, element.width, element.height);
@@ -171,7 +233,9 @@ document.addEventListener("keydown", function (ev) {
         ev.preventDefault();
         //S
         bar_2.down();
+
     } else if (ev.keyCode == 32) {
+        //Pausa con SpaceBar
         ev.preventDefault();
         board.playing = !board.playing;
     }
@@ -182,9 +246,11 @@ board_view.draw();
 
 //Request Animation frame
 window.requestAnimationFrame(controller);
+
+//Tiempo de movimiento de pelota
 setTimeout(function () {
     this.ball.direction = -1;
-}, 4000); 
+}, 4000);
 
 function controller() {
 
